6f45a284d3da7b01c17ac522c2ee36b19b8b20f6
Клим Кузнецов <klimman2000@gmail.com>
2025-07-31 08:06:01 +0200

    continue today

--- tests.py
+++ tests.py
@@ -1,3 +1,5 @@
+import pytest
+
 from main import BooksCollector
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
@@ -22,3 +24,33 @@ class TestBooksCollector:
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+    @pytest.mark.parametrize("book_name, genre", [
+        ("1984", "Фантастика"),
+        ("It", "Ужасы"),
+        ("Шерлок Холмс", "Детективы"),
+    ])
+    def test_set_book_genre_valid_book_and_genre(self, book_name, genre):
+        collector = BooksCollector()
+        collector.add_new_book(book_name)
+        collector.set_book_genre(book_name, genre)
+        assert collector.books_genre[book_name] == genre
+
+    @pytest.mark.parametrize("book_name, invalid_genre", [
+        ("Dracula", "Романтика"),
+        ("Tom & Jerry", "Приключения"),
+    ])
+    def test_set_book_genre_invalid_genre(self, book_name, invalid_genre):
+        collector = BooksCollector()
+        collector.add_new_book(book_name)
+        collector.set_book_genre(book_name, invalid_genre)
+        assert collector.books_genre[book_name] == ""
+
+    def test_get_book_genre_valid_book_and_genre(self):
+        collector = BooksCollector()
+        book_name = collector.add_new_book('Горе от ума')
+        genre = collector.set_book_genre(book_name, 'Комедия')
+        assert collector.get_book_genre(genre) == book_name
+
+
+
